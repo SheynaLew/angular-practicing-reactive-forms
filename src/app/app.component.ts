dd6da3d0733444de31673248ee76b9527ed91c6e
Jenny Lewis-Gandy <jenny.lewis-gandy@voxpopme.com>
2022-11-25 14:20:50 +0000

    adding notes from 209

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,3 +31,31 @@ export class AppComponent implements OnInit {
 
 // In course, the form is set up directly in ngOnInit. I have chosen to create a function to set up the form and then call this in ngOnInit instead because I believe it would make testing easier, and keep the code tidier if ngOnInit were to get busier.
 // In the course, the onSubmit function logs the entire form. I have chose to log the value of the form so that I can see the object more clearly.
+
+
+// Fixing a Bug
+// In the next lecture, we'll add some code to access the controls of our form array:
+
+// *ngFor="let hobbyControl of signupForm.get('hobbies').controls; let i = index"
+
+// This code will fail as of the latest Angular version.
+
+// You can fix it easily though. Outsource the "get the controls" logic into a method of your component code (the .ts file):
+
+// getControls() {
+//   return (<FormArray>this.signupForm.get('hobbies')).controls;
+// }
+// In the template, you can then use:
+
+// *ngFor="let hobbyControl of getControls(); let i = index"
+
+// Alternatively, you can set up a getter and use an alternative type casting syntax:
+
+// get controls() {
+//   return (this.signupForm.get('hobbies') as FormArray).controls;
+// }
+// and then in the template:
+
+// *ngFor="let hobbyControl of controls; let i = index"
+
+// This adjustment is required due to the way TS works and Angular parses your templates (it doesn't understand TS there).
