8ab7584e7cd7f4bd055f75e56185429ebec3bb59
Jenny Lewis-Gandy <jenny.lewis-gandy@voxpopme.com>
2022-11-25 15:11:53 +0000

    211 reactive creating custom validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames = ['Bob', 'Biff'];
 
   // lifecycle hooks
   ngOnInit() {
@@ -24,7 +25,7 @@ export class AppComponent implements OnInit {
   setUpSignUpForm() {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email])
       }),
       'gender': new FormControl(this.genders[1]),
@@ -41,10 +42,20 @@ export class AppComponent implements OnInit {
     (<FormArray>this.signupForm.get('hobbies')).push(control);
   }
 
-
+  // Validators
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return { 'nameIsForbidden': true };
+    }
+    else {
+      return null;
+    }
+  }
 }
 
 // In course, the form is set up directly in ngOnInit. I have chosen to create a function to set up the form and then call this in ngOnInit instead because I believe it would make testing easier, and keep the code tidier if ngOnInit were to get busier.
 // In the course, the onSubmit function logs the entire form. I have chose to log the value of the form so that I can see the object more clearly.
 // One thing from a UX stand point - I would have put an input, then the user inputs their hobby and clicks add hobby, which would add the hobby to the array and create another inpute below it.
+// In custom validators you cannot return false. Return null instead.
+// 'this' is not accessible in the HTML so will cause an error when using custom validators. To get around this, you need to bind 'this' when using the custom validators so that the HTML knows to look at the variable referred to in the ts.
 
